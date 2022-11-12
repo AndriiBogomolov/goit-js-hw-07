76bf483105e9105cc67e7409f7840f2e638a4567
Andrii Bogomolov <bogomolov.andrii@gmail.com>
2022-11-12 22:39:30 +0100

    Update hw-07

---
+++ js/02-lightbox.js
@@ -0,0 +1,26 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+const gallery = document.querySelector(".gallery");
+const items = [];
+
+galleryItems.forEach((elem) => {
+  const galleryLink = document.createElement("a");
+  galleryLink.className = "gallery__link";
+  galleryLink.href = elem.original;
+  const galleryImage = document.createElement("img");
+
+  galleryImage.className = "gallery__image";
+  galleryImage.src = elem.preview;
+  galleryImage.setAttribute("title", elem.description);
+  galleryImage.alt = elem.description;
+
+  galleryLink.append(galleryImage);
+  items.push(galleryLink);
+});
+
+gallery.append(...items);
+
+new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+});
